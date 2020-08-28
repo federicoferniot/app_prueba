82c64482f1901b2572c45900fa040358c44fe9ed
Ferniot Federico <feder.fer.93@gmail.com>
2020-08-27 23:43:30 -0300

    server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express'); const app = express(); const path = require('path'); 
-app.use(express.static(__dirname + '/angularapp')); 
+app.use(express.static(__dirname + '/src')); 
 app.listen(process.env.PORT || 8080); 
 app.get('/*', function(req, res) { 
-res.sendFile(path.join(__dirname + '/angularapp/src/index.html')); }); 
+res.sendFile(path.join(__dirname + '/index.html')); }); 
 console.log('Build successful!!'); 
